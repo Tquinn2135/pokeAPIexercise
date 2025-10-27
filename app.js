da2f179e032500339d65d5e8f7e6b6e9266a36f1
TQuinn2135 <Tquinn2135@gmail.com>
2025-10-27 10:27:00 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,31 @@
+
+(async ()=> {
+
+     const randomPokemon = Math.floor(Math.random() * 150)
+
+     const getJsonFrom = async url => {
+          const response = await fetch(url)
+          return await response.json()
+     }     
+     
+     const getRandomPokemon = async pokemon => {
+          const url = 'https://pokeapi.co/api/v2/pokemon/' + randomPokemon
+          const json = await getJsonFrom(url)
+          return json
+     }
+
+     const renderPokemon = (pokemon) => {
+          const img = document.createElement('img')
+          img.src = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${randomPokemon}.png`
+          img.alt = pokemon.name
+          const div = document.querySelector('div')
+          div.appendChild(img)
+     }
+      
+     const pokemon = await getRandomPokemon()
+     renderPokemon(pokemon)
+
+     //console.log(pokemon)
+
+})()
+   
